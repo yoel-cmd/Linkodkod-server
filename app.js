e430258fb69909a13e7717d00fd6e2caec6ff0bd
yoel ider <yoeli5746@gmail.com>
2025-09-08 17:55:26 +0300

    I finished the server side and got to the routing requirement.

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./src/routes/posts.js";
 import cors from "./src/middlewares/CORS.js";
+import { error } from "console";
 
 dotenv.config();
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors);
 
 app.use("/", router);
 
+app.use((req, res) => {
+  res.status(404).json({ error: "The path does not exist." });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Listening to the port : ${PORT}`);
 });
+
+
+
