333f1c404a54c1c974db71f4bc133b3158c629a4
yoel ider <yoeli5746@gmail.com>
2025-09-08 15:25:57 +0300

    I added MW for CORS errors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./src/routes/posts.js"
+import router from "./src/routes/posts.js";
+import cors from "./src/middlewares/CORS.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 
 app.use("/", router);
 
---
+++ src/middlewares/CORS.js
@@ -0,0 +1,15 @@
+export default function cors(req, res, next) {
+ 
+  res.header("Access-Control-Allow-Origin", "http://localhost:5174");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+}
