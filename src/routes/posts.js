aa93c2c2f4d085e7640024aa6c29adea053203ec
yoel ider <yoeli5746@gmail.com>
2025-09-09 09:49:26 +0300

    I made a path including all its dependencies to get a post by id.

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {getAllPosts} from "../controllers/posts.controller.js"
+import {getAllPosts,getPostById} from "../controllers/posts.controller.js"
 
 const router = Router();
 
 router.get("/post",getAllPosts);
+router.get("/post/:id",getPostById);
+
 
 
 export default router;
